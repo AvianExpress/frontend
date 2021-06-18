13352b5645cfe26205ae724416b01185dce7b9b0
Ser4eese <miden99@gmail.com>
2021-06-18 16:50:29 +0700

    last

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from "bootstrap-vue"
 import router from './router'
 import './axios'
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,6 +3,7 @@
 
   <div class  = "Users">
   <h1 >Пользователи</h1>
+  <table></table>
   <ul class="list">
     <template  v-for="user in users">
     <li :key="user.id">Имя: {{user.name}} Адрес: {{user.address}}</li>
@@ -10,11 +11,34 @@
   </ul>
   </div>
    <div>
-    <b-table striped hover :items="users"></b-table>
+    <b-container fluid>
+    <b-table 
+    :items="users"
+    :fields="fields"
+    :select-mode="seletMode"
+     responsive="sm"
+     ref="selectableTable"
+     selectable
+     @row-selected="onRowSelected"
+    >
+ </b-table>
+    </b-container>
+<div>
+  <b-input-group prepend="Changefolder" class="mb-2">
+    <b-form-input aria-label="Firstname" :value="selected[0].name" placeholder="Firstname"></b-form-input>
+    <b-form-input aria-label="Lastname" :value="selected[0].surname" placeholder="Lastname"></b-form-input>
+    <b-form-input aria-label="Phone" :value="selected[0].phone" placeholder="Phone"></b-form-input>
+    <!--<b-form-input aria-label="Address" :value="selected[0].address" enable=false placeholder="Address"></b-form-input>-->
+    <b-button size="" text="Button" variant="success" v-on:click="change">Change</b-button>
+  </b-input-group>
+</div>
   </div>
+  <h3>{{ selected}}</h3>
+
   <button class="button" v-on:click="logout">LogOut</button>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios'
@@ -25,13 +49,35 @@ export default {
       localStorage.removeItem('access_token');
       //localStorage.setItem('access_token', " ");
       this.$router.go()
-    }
+    },
+    onRowSelected(items) {
+        this.selected = items;
+      },
+      async change(){
+        await axios.put('resp/'+this.selected.id, this.selected);
+      }
+      
   },
   components: {
   },
   data(){
     return {
       users: null,
+      selected:[{
+        id: "",
+        name:"",
+        surname:"",
+        address: "",
+        phone:"",
+      }],
+      seletMode: 'single',
+      name: "1",
+      fields:[
+          { key: 'name', label: 'First name', sortable: true, sortDirection: 'desc' },
+          { key: 'surname', label: 'Last name', sortable: true, class: 'text-center' },
+          { key: 'phone', label: 'Phone', sortable: true, class: 'text-center' },
+          //{ key: 'address', label: 'Person address', sortable: true, class: 'text-center' },
+      ],
     }
   },
   async created(){
@@ -53,5 +99,8 @@ export default {
   .subtitle {
     text-shadow: 4px 4px 4px rgba(0, 0, 0, 0.7);
     font-size: 30px;
+  }
+  .button{
+    margin: 20px;
   }
 </style>>
